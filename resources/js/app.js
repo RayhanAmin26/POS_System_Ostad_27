01a1e2f4ea6e18d28ce2fc2622c65a50f2e38a31
RayhanAmin26 <rayhanamin.lm2001@gmail.com>
2025-09-06 21:37:42 +0600

    app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
-import './bootstrap';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { InertiaProgress } from '@inertiajs/progress'
+import axios from 'axios'
+
+InertiaProgress.init()
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
+axios.interceptors.request.use(config => {
+    const token = localStorage.getItem('token')
+    if(token) config.headers.Authorization = `Bearer ${token}`
+    return config
+})
+
+createInertiaApp({
+    resolve: name => import(`./Pages/${name}.vue`),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
+})
+
