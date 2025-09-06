d53bcca0493336daaaf045a44d79320cbb1642e1
RayhanAmin26 <rayhanamin.lm2001@gmail.com>
2025-09-06 22:23:07 +0600

    auth

--- config/auth.php
+++ config/auth.php
@@ -34,13 +34,19 @@ return [
     | Supported: "session"
     |
     */
-
     'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
+    'web' => [
+        'driver' => 'session',
+        'provider' => 'users',
     ],
+
+    'api' => [
+        'driver' => 'jwt',
+        'provider' => 'users',
+        'hash' => false,
+    ],
+],
+
 
     /*
     |--------------------------------------------------------------------------
