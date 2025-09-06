bc6ff07647d543aaeaa6588ef0e636192a59df9f
RayhanAmin26 <rayhanamin.lm2001@gmail.com>
2025-09-06 21:25:57 +0600

    brands

--- database/migrations/2025_09_06_152304_create_brands_table.php
+++ database/migrations/2025_09_06_152304_create_brands_table.php
@@ -12,9 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('brands', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+    $table->id();
+    $table->string('name');
+    $table->timestamps();
+});
+
     }
 
     /**
