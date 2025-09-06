adadb0a4170f6c384f14f1740f003edd6665ab72
RayhanAmin26 <rayhanamin.lm2001@gmail.com>
2025-09-06 22:25:16 +0600

    createPro

--- database/migrations/2025_09_06_152304_create_products_table.php
+++ database/migrations/2025_09_06_152304_create_products_table.php
@@ -1,34 +1,20 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+return new class extends Migration {
+    public function up(){
         Schema::create('products', function (Blueprint $table) {
-    $table->id();
-    $table->string('name');
-    $table->foreignId('category_id')->constrained()->onDelete('cascade');
-    $table->foreignId('brand_id')->constrained()->onDelete('cascade');
-    $table->decimal('price', 10, 2);
-    $table->integer('quantity');
-    $table->string('image')->nullable();
-    $table->timestamps();
-});
-
+            $table->id();
+            $table->string('name');
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('brand_id')->constrained()->cascadeOnDelete();
+            $table->decimal('price', 10, 2)->default(0);
+            $table->integer('quantity')->default(0);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
     }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('products');
-    }
+    public function down(){ Schema::dropIfExists('products'); }
 };
