ad0368810c1901a5e29a2824f3a806d88342fd43
RayhanAmin26 <rayhanamin.lm2001@gmail.com>
2025-09-06 22:24:53 +0600

    create_Brands

--- database/migrations/2025_09_06_152304_create_brands_table.php
+++ database/migrations/2025_09_06_152304_create_brands_table.php
@@ -1,29 +1,15 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+return new class extends Migration {
+    public function up(){
         Schema::create('brands', function (Blueprint $table) {
-    $table->id();
-    $table->string('name');
-    $table->timestamps();
-});
-
+            $table->id();
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
     }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('brands');
-    }
+    public function down(){ Schema::dropIfExists('brands'); }
 };
